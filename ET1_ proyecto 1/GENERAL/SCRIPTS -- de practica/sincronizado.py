ebeaf6bb7ee18303c5dd7654b8409e6729fa6f43
Octavio Osorio Trujillo <octavioosoriotrujillo@gmail.com>
2025-08-21 13:26:52 -0600

    F3 Actualizado 21 de agosto 2025

--- ET1_ proyecto 1/GENERAL/SCRIPTS -- de practica/sincronizado.py
+++ ET1_ proyecto 1/GENERAL/SCRIPTS -- de practica/sincronizado.py
@@ -1,3 +1,7 @@
+
+import ipywidgets as widgets
+from IPython.display import display, HTML
+
 # mi_script.py
 def super_resumen(archivos_lista):
     """
@@ -6,4 +10,25 @@ def super_resumen(archivos_lista):
     """
     for archivo in archivos_lista:
         print(archivo)
-        
+
+        if archivo.endswith(".csv"):
+         mensajes = ["Cargando imagen...", "Aplicando filtros...", "Corrigiendo colores...", "Guardando copia...", "✅ Imagen lista"]
+
+        elif archivo.endswith(".png"):
+            mensajes = ["Cargando imagen...", "Aplicando filtros...", "Corrigiendo colores...", "Guardando copia...", "✅ Imagen lista"]
+        elif archivo.endswith(".txt"):
+            mensajes = ["Leyendo TXT...", "Buscando errores...", "Transformando datos...", "Escribiendo nuevo archivo...", "✅ Texto procesado"]
+        else:
+            mensajes = ["Generando archivo...", "Agregando contenido...", "Insertando gráficos...", "Guardando PDF...", "✅ Documento creado"]
+        bloque_texto = "<br>".join(mensajes)
+
+        mensajes_y_checkbox = widgets.VBox([
+            widgets.HBox([widgets.HTML(f"<b>📄 {archivo}</b>")]),
+            widgets.HTML(f"<div style='font-family:Courier New, monospace; font-size:13px; color:#2D6FA4; overflow:auto; height:200px;width:500px'>{bloque_texto}</div>")
+            #widgets.HBox([widgets.HTML(f"<b>📄 {archivo}</b>"),checkbox])
+
+        ])
+
+        display( mensajes_y_checkbox)
+
+
