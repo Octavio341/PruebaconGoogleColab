282438633efb86bf27f994c514a3210d99502427
Octavio Osorio Trujillo <octavioosoriotrujillo@gmail.com>
2025-08-21 12:11:05 -0600

    actualizacion 21 f1

---
+++ ET1_ proyecto 1/GENERAL/SCRIPTS -- de practica/sincronizado.py
@@ -0,0 +1,8 @@
+# mi_script.py
+def mostrar_archivos_form(archivos_lista):
+    """
+    Recibe una lista de archivos y los muestra en un formulario interactivo
+    con checkboxes.
+    """
+    for archivo in archivos_lista:
+        print(archivo)
