4c263728a4b6a606aa2a70525d9f063ac664cdbe
Octavio Osorio Trujillo <octavioosoriotrujillo@gmail.com>
2025-08-22 15:36:50 -0600

    F6 Actualizado 22 de agosto 2025

--- ET1_ proyecto 1/GENERAL/SCRIPTS -- de practica/sincronizado.py
+++ ET1_ proyecto 1/GENERAL/SCRIPTS -- de practica/sincronizado.py
@@ -82,7 +82,7 @@ def super_resumen(archivos_lista):
                             sep=r'\s+',
                             names=["StationID","StationName","Date","D1","D2","D3","D4","D5","D6",
                                     "D7","D8","D9","D10","D11","D12"],
-                            engine='python', skiprows=2, na_values="9999")
+                            engine='python', skiprows=1, na_values="9999")
         
         matriz_toga["Archivo"] = nombre  # agregamos columna con nombre de archivo
         #bloques.append(matriz_toga.head())  # guardamos DataFrame de ejemplo
@@ -90,6 +90,78 @@ def super_resumen(archivos_lista):
         #display(matriz_toga)
         lista_fecha= list()
         lista_datos=list()
+
+        # Crear la fecha de control
+        fecha = str(matriz_toga["Date"][0])
+        fecha_control=datetime.datetime(int(fecha[:4]),1,1)
+        mensajes.append(f"Archivo: {nombre} cargado se han encontrado datos del anio {fecha_control.strftime('%Y')}")
+        bloque_texto = "<br>".join(mensajes)
+
+        #####################################  ==========================
+        ind = 0
+        hora_control=1
+        dias_faltantes=0
+        dias_existentes=0
+        anio_final=int(fecha[:4])+1
+        # Generar una a una las fechas y compararlas con el dataframe
+        while fecha_control.year < anio_final:
+        ###################################################
+
+            if ind < len(matriz_toga.index):
+
+                # Cargar la fecha del dataframe
+                fecha = str(matriz_toga['Date'][ind])
+
+            # Verificar si la fecha de control y la fecha del dataframe son iguales
+            if fecha == fecha_control.strftime('%Y%m%d')+str(hora_control):
+
+                # Contar el dia existente
+                dias_existentes = dias_existentes+1
+
+            else:
+        ###################################################
+                # Si la fecha de control y la fecha del dataframe no son iguales, avisar al usuario y contar la linea faltante
+                #print("No se encontr'o la fecha "+fecha_control.strftime("%Y%m%d")+str(hora_control))
+                mensajes.append(f"No se encontro la fecha {fecha_control.strftime('%Y%m%d')}{str(hora_control)}")
+                bloque_texto = "<br>".join(mensajes)
+                dias_faltantes=dias_faltantes+1
+
+                # Avanzar con la fecha de control hasta que sea igual a la del dataframeo se acabe el año
+        ###################################################
+                while fecha != fecha_control.strftime('%Y%m%d')+str(hora_control) and fecha_control.year < anio_final:
+                    # Avanzar la fecha de control
+                    if hora_control == 1:
+                        hora_control = 2
+                    else:
+                        hora_control = 1
+                        fecha_control=fecha_control+timedelta(days=1)
+                
+                    if fecha == fecha_control.strftime('%Y%m%d')+str(hora_control):
+                        # Si la fecha de control y la fecha del dataframe son iguales, contarlo
+                        dias_existentes=dias_existentes+1
+                    else:
+                        # Si la fecha de control y la fecha del dataframe no son iguales, avisar al usuario y contar la linea faltante
+                        mensajes.append(f"No se encontro la fecha {fecha_control.strftime('%Y%m%d')} {str(hora_control)}")
+                        bloque_texto = "<br>".join(mensajes)
+                        #print("No se encontr'o la fecha "+fecha_control.strftime("%Y%m%d")+str(hora_control))
+                        dias_faltantes=dias_faltantes+1
+        ###################################################
+            # Incrementar el indice del dataframe
+            ind = ind + 1
+        ###################################################
+            # Avanzar la fecha de control
+            if hora_control == 1:
+                hora_control = 2
+            else:
+                hora_control = 1
+                fecha_control=fecha_control+timedelta(days=1)
+
+        ###################################################
+        datos_validos=(dias_faltantes+dias_existentes)*12-matriz_toga.isna().sum().sum()
+        mensajes.append(f"Hay {str(datos_validos)} datos validos ({str("{0:.2f}".format(datos_validos*100/((dias_faltantes+dias_existentes)*12)))}% ) y {str(matriz_toga.isna().sum().sum())} datos nulos ({str("{0:.2f}".format(matriz_toga.isna().sum().sum()*100/((dias_faltantes+dias_existentes)*12)))}%)")
+        bloque_texto = "<br>".join(mensajes)
+        #print("Hay "+str(datos_validos)+" datos validos ("+str("{0:.2f}".format(datos_validos*100/((dias_faltantes+dias_existentes)*12)))+"%) y "+str(matriz_toga.isna().sum().sum())+" datos nulos ("+str("{0:.2f}".format(matriz_toga.isna().sum().sum()*100/((dias_faltantes+dias_existentes)*12)))+"%)")
+        ##################################### ==========================
         for filas_dat in matriz_toga.index:
           fecha = str(matriz_toga["Date"][filas_dat])
           #fecha es =  195701011
@@ -133,7 +205,7 @@ def super_resumen(archivos_lista):
                 widgets.HTML(f"<div style='font-family:Courier New, monospace; font-size:13px; color:#2D6FA4; height:200px;width:500px'>{bloque_texto}</div>")
             ])
         ], layout=widgets.Layout(border="2px solid #2D6FA4", border_radius="10px",
-                                padding="10px", margin="5px", width="900px",align_items="flex-start"))
+                                padding="10px", margin="5px", width="1100px",align_items="flex-start"))
         #display(contenedor_final
         contenedor_final.layout.flex = "0 0 auto"
         contenedor_general.append(contenedor_final)  # acumulamos cada bloque
@@ -172,13 +244,13 @@ def super_resumen(archivos_lista):
 
     # Crear cajas con contenido variado
     box1 = make_box("Resumen general",contenedor_general)
-    box1.layout.width ='900px'
+    box1.layout.width ='1200px'
     box1.layout.height ='500px'
     box2 = make_box("Opciones ",opciones_boton)
-    box2.layout.width ='900px'
+    box2.layout.width ='1200px'
     box2.layout.height ='100px'
     box3 = make_box("Cargar Archivo",salida_resultados)
-    box3.layout.width ='900px'
+    box3.layout.width ='500px'
     box3.layout.height ='600px'
     fila = widgets.VBox([box2,box1])
     fila2 = widgets.HBox([box3])
